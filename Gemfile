41348d5eea6a920f404fa313e7a1d1081d4b9aa1
Brian Gates <briandotgates@gmail.com>
2013-06-17 21:20:43 -0400

    Add user index, paginate, and make admins capable of destroying other users

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ end
 gem 'rails', '4.0.0.rc2'
 gem 'bootstrap-sass', '2.3.1.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 gem 'sass-rails', '4.0.0.rc2'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+  before_action :find_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :signed_in_user, only: [ :index, :edit, :update ]
+  before_action :authorize_user, only: [ :edit, :update ]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -14,12 +24,52 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    
   end
 
+  def edit
+    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes user_params
+      sign_in @user
+      redirect_to @user, flash: { success: 'That worked.' }
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: 'Please sign in.'
+      end
+    end
+
+    def authorize_user
+      unless current_user?(@user)
+        store_location
+        redirect_to signin_url, notice: 'Please sign in.' 
+      end
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 end
